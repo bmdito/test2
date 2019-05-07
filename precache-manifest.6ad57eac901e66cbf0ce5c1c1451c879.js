1d80f013f05b725568e733cf47ddf0b645425654
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-05-07 15:10:59 -0700

    Updates

---
+++ precache-manifest.6ad57eac901e66cbf0ce5c1c1451c879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccc1ec22532629b2f569006e0382651",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "474acf6ad175a6bb5e53",
+    "url": "/test2/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "474acf6ad175a6bb5e53",
+    "url": "/test2/static/js/main.1c490839.chunk.js"
+  },
+  {
+    "revision": "872033a9efb6eb9fe611",
+    "url": "/test2/static/js/runtime~main.bb481ce1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
